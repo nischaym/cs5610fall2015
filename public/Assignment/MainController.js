87a6b24367111e9076f8e2e1767f9c7ea9eb349a
nischay maridevegowda <nischaym@ccs.neu.edu>
2015-10-16 01:35:58 -0400

    adding angular

---
+++ public/Assignment/MainController.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    angular
+        .module("FormBuilderApp")
+        .controller("MainController", MainController);
+
+    function MainController($scope , $location){
+        
+        $scope.$location = $location;
+    
+        
+    }
+
+})();
