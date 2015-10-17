b906c9159684d6f884a7092ca6d20c90e67022e9
nischay maridevegowda <nischaym@ccs.neu.edu>
2015-10-17 19:16:25 -0400

    released code

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+ 
+ 
+ app.use(express.static(__dirname + '/public'));
+ 
+var address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port    = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+//app.get('/', function (req, res) {
+//  res.send('Hello World')
+//})
+
+app.listen(port,address)
