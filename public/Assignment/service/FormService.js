bd45edffd4bc224720154251a7835c69adf7185d
nischay maridevegowda <nischaym@ccs.neu.edu>
2015-10-22 01:56:42 -0400

    adding formservice

---
+++ public/Assignment/service/FormService.js
@@ -0,0 +1,38 @@
+﻿(function () {
+
+    angular
+        .module("FormBuilderApp")
+        .factory("FormController", FormController)
+
+    function FormController ()
+    {
+        var forms = [
+            {formid : "" , userid : ""},
+            {}
+        ];
+
+        var formservice =
+        {
+            createFormForUser: createFormForUser,
+            findAllFormsForUser: findAllFormsForUser,
+            deleteFormById: deleteFormById,
+            updateFormById: updateFormById
+        }
+        return formservice;
+
+        function createFormForUser(userid, form, cb_fn)
+        {
+            form.userid = userid;
+            form.formid = forms.length + 1;
+            forms.push(form);
+            cb_fn(form);
+        }
+
+        function findAllFormsForUser ()
+        {
+
+        }
+    }
+
+
+})();
