27cf188bf2f34eda1c961573a6326c8e47f0194e
nischay maridevegowda <nischaym@ccs.neu.edu>
2015-11-01 22:24:21 -0500

    trip still working

---
+++ public/TripHorsePower/config.js
@@ -0,0 +1,41 @@
+﻿(function () {
+
+    angular
+        .module("TripHorsePower")
+        .config(configure);
+
+    function configure($routeProvider) {
+        $routeProvider
+        .when("/home", {
+            templateUrl: "home/home.view.html"
+        })
+            /*
+        .when("/register", {
+            templateUrl: "register/register.view.html",
+            controller : "RegisterController"
+        })
+        .when("/profile", {
+            templateUrl: "profile/profile.view.html",
+            controller: "ProfileController"
+        })
+        .when("/login", {
+            templateUrl: "login/login.view.html",
+            controller : "LoginController"
+        })
+        .when("/admin", {
+            templateUrl: "admin/admin.view.html"
+        })
+        .when("/forms", {
+            templateUrl: "form/form.view.html",
+            controller : "FormController"
+        })
+
+        .when("/form-fields", {
+            templateUrl: "form/form-fields.view.html",
+            controller: "FormController"
+        })*/
+        .otherwise({
+            redirectTo: "/home"
+        });
+    }
+})();
