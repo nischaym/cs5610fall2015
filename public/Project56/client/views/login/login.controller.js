27f91a3c955c3c4afd51a4361aa0c06b5a9b93f4
nischay maridevegowda <nischaym@ccs.neu.edu>
2015-12-07 13:35:24 -0500

    fixed register page

--- public/Project56/client/views/login/login.controller.js
+++ public/Project56/client/views/login/login.controller.js
@@ -6,27 +6,38 @@
     function LoginController($scope , $rootScope, $location,UserService) {
 
         $rootScope.user;
-        //$scope.user = {username:"",password:""};
+        $scope.user = {username:"",password:""};
         $scope.login = login;
-
+        //$scope.user.username = "";
+        //$scope.user.username = "";
         /* function for the check of login*/
         function login (username, password)
         {
-            UserService.findUserByUsernameAndPassword(username, password).then(function(response){
-                var user = response;
-                console.log(response);
-                if(user == null)
-                {
-                    alert("username not present");
-                }
-                else
-                {
-                    $rootScope.user = user[0];
-                    $rootScope.user.logged = true;
-                    $rootScope.user.globalusername = $scope.user.username;
-                    $location.url('/profile/'+$rootScope.user._id);
-                }
-            });
+            if(username == null || password == null)
+            {
+                alert("Please Enter Details");
+            }
+            else
+            {
+                UserService.findUserByUsernameAndPassword(username, password).then(function(response){
+                    var user = response;
+                    console.log(response);
+                    if(user.length == 0)
+                    {
+                        alert("username password not Matching");
+                    }
+                    else
+                    {
+                        $rootScope.user = user[0];
+                        $rootScope.user.logged = true;
+                        $rootScope.user.globalusername = $scope.user.username;
+                        $scope.user.username = "";
+                        $scope.user.password = "";
+                        $location.url('/profile/'+$rootScope.user._id);
+
+                    }
+                });
+            }
         }
     }
 })();
--- public/Project56/client/views/register/register.controller.js
+++ public/Project56/client/views/register/register.controller.js
@@ -8,19 +8,25 @@
 
         $scope.register = register;
         $rootScope.user ;//= {username:"",password:"",email:"",userid:"",logged:false,globalusername:""};
+        $scope.user = {username:"",password:"",city:"",state:"",firstname:"",lastname:""};
         function register()
         {
-            var newUser =
+            if($scope.user.username == "" || $scope.user.password =="" || $scope.user.email == "" || $scope.user.city == "" || $scope.user.state =="" ||$scope.user.firstname == "" || $scope.user.lastname == "")
             {
-                username : $scope.user.username,
-                password : $scope.user.password,
-                email: $scope.user.email,
-                city:$scope.user.city,
-                state:$scope.user.state,
-                firstname:$scope.user.firstname,
-                lastname:$scope.user.lastname
-            };
-            console.log(newUser);
+                alert('Please enter all the details');
+            }
+            else
+            {
+                var newUser =
+                {
+                    username : $scope.user.username,
+                    password : $scope.user.password,
+                    email: $scope.user.email,
+                    city:$scope.user.city,
+                    state:$scope.user.state,
+                    firstname:$scope.user.firstname,
+                    lastname:$scope.user.lastname
+                };
                 UserService.createUser(newUser).then(function (response) {
 
                     newUser = response;
@@ -33,9 +39,11 @@
                     //$rootScope.user.userid = newUser._id;
                     $rootScope.user.logged = true;
                     $rootScope.user.globalusername = newUser.username;
+                    $scope.user = {username:"",password:"",city:"",state:"",firstname:"",lastname:""};
                     $location.url('/profile/'+newUser._id);
                 });
+            }
         }
-    };
+    }
 })();
 
